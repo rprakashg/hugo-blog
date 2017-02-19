0c228bbaa1676e50df5d94895126ecf9e64e4419
Ram Gopinathan <rprakashg@outlook.com>
2017-02-18 21:07:25 -0800

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages --force
-
-echo "Removing existing files"
-rm -rf public/*
-
 echo "Generating site"
 hugo
 
-echo "Updating gh-pages branch"
-#cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+# Add changes to git.
+git add -A
 
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+git push origin master
 git subtree push --prefix=public git@github.com:rprakashg/hugo_gh_blog.git gh-pages
